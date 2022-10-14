b1cd987d0e3bf3ce5372930e5abe5938ec77f5dc
oksana <oksa-blokhi@yandex.ru>
2022-10-14 20:40:12 +0300

    Task011new

--- Task011/Program.cs
+++ Task011/Program.cs
@@ -6,16 +6,56 @@ if(a<1||a>7)
     Console.WriteLine("Такого дня нет:"); 
 }    
     if(a==1)
-     Console.WriteLine("Понедельник");
+    Console.WriteLine("Понедельник");
     if(a==2)
-     Console.WriteLine("Вторник");
+    Console.WriteLine("Вторник");
     if(a==3)
-     Console.WriteLine("Среда");
+    Console.WriteLine("Среда");
     if(a==4)
-     Console.WriteLine("Четверг");
+    Console.WriteLine("Четверг");
     if(a==5)
-     Console.WriteLine("Пятница");
+    Console.WriteLine("Пятница");
     if(a==6)
-     Console.WriteLine("Суббота");
+    Console.WriteLine("Суббота");
     if(a==7)
-     Console.WriteLine("Воскресенье");
+    Console.WriteLine("Воскресенье");
+
+// Console.WriteLine("Введите 1 число");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+//  if(a<1||a>7)
+
+//     switch(a)
+//     {
+//     case 1:
+//         Console.WriteLine("Понедельник");
+//     break;
+
+//     case 2:
+//         Console.WriteLine("Вторник");
+//     break;
+
+//     case 3:
+//         Console.WriteLine("Среда");
+//     break;
+
+//     case 4:
+//         Console.WriteLine("Четверг");
+//     break;
+
+//     case 5:
+//         Console.WriteLine("Пятница");
+//     break;
+
+//     case 6:
+//         Console.WriteLine("Суббота");
+//     break;
+
+//     case 7:
+//         Console.WriteLine("Воскресенье");
+//     break;
+
+//     default:
+//         Console.WriteLine("Такого дня нет");
+//     break;    
+//     }
