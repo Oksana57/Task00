1c9e259cae92cdb3113cf907752d6a2a1399697f
oksana <oksa-blokhi@yandex.ru>
2022-10-14 20:52:31 +0300

    task05

---
+++ Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a<1) 
+Console.WriteLine("Вы ввели недопустимое число");
+
+else
+{
+    int count = -a;
+    while (count<=a)
+    {
+    Console.WriteLine($"{count}");
+    count ++;
+    }
+
+}
