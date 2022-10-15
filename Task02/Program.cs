e819ab8e43b9ce7f3c73ab1a04abb54def46c1e3
oksana <oksa-blokhi@yandex.ru>
2022-10-15 19:43:02 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine($"{a} - максимальное число");
+    Console.WriteLine($"{b} - минимальное число");
+}
+else
+{
+    Console.WriteLine($"{b} - максимальное число");
+    Console.WriteLine($"{a} - минимальное число");
+}
