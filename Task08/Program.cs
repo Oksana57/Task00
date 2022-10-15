a47d4e6d3dac12020e683239f602f20a0cbea653
oksana <oksa-blokhi@yandex.ru>
2022-10-15 20:43:31 +0300

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+if(N<2)
+{
+    Console.WriteLine("Некорректное число");
+}
+else
+{
+    while (count<N-1)
+    {
+        count+=2;
+        Console.WriteLine($"{count}");
+    }
+}
