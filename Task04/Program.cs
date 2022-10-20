97cde2d1e609dc7e2bb2f67f3f87b52032afa176
oksana <oksa-blokhi@yandex.ru>
2022-10-20 10:52:27 +0300

    Task 02, 04, 06, 08

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Введите 1 число");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Введите 1 число");
 int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите 2 число");
@@ -10,7 +17,6 @@ int c = Convert.ToInt32(Console.ReadLine());
 
 int max = a;
 
-if(a>max) max = a;
 if(b>max) max = b;
 if(c>max) max = c;
 
--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,15 +1,21 @@
-﻿Console.WriteLine("Введите число");
-int N = Convert.ToInt32(Console.ReadLine());
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
 
 int count = 0;
 
-if(N<2)
+if(n<2)
 {
     Console.WriteLine("Некорректное число");
 }
 else
 {
-    while (count<N-1)
+    while (count<n-1)
     {
         count+=2;
         Console.WriteLine($"{count}");
