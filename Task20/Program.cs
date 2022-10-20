81879ecda2c040450ec927ead887da79900950c8
oksana <oksa-blokhi@yandex.ru>
2022-10-20 21:37:54 +0300

    Task20 new

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -10,13 +10,17 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
+double Distance(int xa, int ya, int xb, int yb)
+{
+double d = Math.Sqrt(((xa-xb)*(xa-xb))+((ya-yb)*(ya-yb)));
+return d;
+}
+
 Console.WriteLine("Введите координаты точкиc1");
 Console.Write("x1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y1: ");
 int y1 = Convert.ToInt32(Console.ReadLine());
-
-
 
 Console.WriteLine("Введите координаты точки2");
 Console.Write("x2: ");
@@ -24,11 +28,11 @@ int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 
-double Distance(int xa, int ya, int xb, int yb)
-{
-double d = Math.Sqrt(((xa-xb)*(xa-xb))+((ya-yb)*(ya-yb)));
-return d;
-}
+// double Distance(int xa, int ya, int xb, int yb)
+// {
+// double d = Math.Sqrt(((xa-xb)*(xa-xb))+((ya-yb)*(ya-yb)));
+// return d;
+// }
 
 double distance =  Distance(x1, y1, x2, y2);
 
