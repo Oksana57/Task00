2729a01dedce842243992a6a2d7f923b98a9a712
oksana <oksa-blokhi@yandex.ru>
2022-11-07 22:29:11 +0300

    Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -2,3 +2,54 @@
 // выводит массив из 8 элементов, заполненный
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
+
+// void FillArray (int[] arr)
+// {
+    
+//     for(int i = 0; i<arr.Length;i++)
+//     {
+//         arr[i] = new Random().Next(0,2);
+       
+//     }
+// }
+
+// void PrintArray (int[] arr)
+// {
+//     Console.Write("[");
+//     for(int i = 0; i<arr.Length;i++)
+//     {
+//         if(i==arr.Length-1)Console.Write($"{arr[i]}");
+//         else Console.Write($"{arr[i]}, ");
+//     }   
+    
+//     Console.WriteLine("]");
+// }
+// int [] array = new int [8];
+// FillArray(array);
+// PrintArray(array);
+
+int[] CreateArray(int len)
+{
+    int[] arr = new int[len];
+       for (int i = 0; i<len; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i<arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i<arr.Length-1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+PrintArray(array);
