5bad0e31f32a287f53f2d469a1c45c5613931dd8
Oksana Blokhina <oksa-blokhi@yandex.ru>
2022-11-12 17:21:03 +0300

    Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
 double[] CreateArrayRndDouble(int size, int min, int max)
 {
@@ -11,3 +13,60 @@ double[] CreateArrayRndDouble(int size, int min, int max)
     }
     return array;
 }
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double MinDigit(double[] array)
+{
+    int i = 0;
+    double mindigit = array[i];
+    while (i < array.Length)
+    {
+        
+        if (array[i] < mindigit) mindigit = array[i];
+        i++;
+    }
+    return mindigit;
+}
+
+double MaxDigit(double[] array)
+{
+    int i = 0;
+    double maxdigit = array[i];
+    while (i < array.Length)
+    {
+        if (array[i] > maxdigit) maxdigit = array[i];
+        i++;
+    }
+    return maxdigit;
+}
+
+
+double[] arr = CreateArrayRndDouble(5, -100, 100);
+PrintArray(arr);
+double minDigit = MinDigit(arr);
+double maxDigit = MaxDigit(arr);
+
+void SumMinMax(double min, double max)
+{
+    double minDigit = MinDigit(arr);
+    double maxDigit = MaxDigit(arr);
+    double sum = minDigit + maxDigit;
+    sum = Math.Round(sum, 1);
+    Console.WriteLine($"Сумма минимального и максимального значения массива = {sum}");
+}
+SumMinMax(minDigit, maxDigit);
+// double sumMinMax = SumMinMax(arr)
+// double minDigit = MinDigit(arr);
+// Console.WriteLine($"Минимальное число в массиве = {minDigit}");
+// double maxDigit = MaxDigit(arr);
+// Console.WriteLine($"Максимальное число в массиве = {maxDigit}");
