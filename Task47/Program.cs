8bd3db530ae2b426f20c4242dfbed04dd57076f8
Oksana Blokhina <oksa-blokhi@yandex.ru>
2022-11-18 20:15:29 +0300

    Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreatMatrixRndDouble(int rows, int columns, double min, double max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            double num = rnd.NextDouble() * (max - min) + min;
+            matrix[i, j] = Math.Round(num, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(double[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // Console.Write($"{matrix[i, j]}");
+            if (i < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("");
+
+    }
+}
+
+Console.WriteLine("Введите m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число min ");
+double min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число max");
+double max = Convert.ToInt32(Console.ReadLine());
+
+double[,] creatmatrixrnddoubl = CreatMatrixRndDouble(m, n, min, max);
+PrintArray(creatmatrixrnddoubl);
